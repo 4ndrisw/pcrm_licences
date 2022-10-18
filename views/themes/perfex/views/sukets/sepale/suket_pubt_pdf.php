820ebeb552fbfe9983fcef8a91c5a776c3510e25
4ndr1sw <4ndr1sw@gmail.com>
2022-10-18 07:40:28 +0700

    Update suket_pubt_pdf.php

--- views/themes/perfex/views/sukets/sepale/suket_pubt_pdf.php
+++ views/themes/perfex/views/sukets/sepale/suket_pubt_pdf.php
@@ -8,10 +8,11 @@ $dimensions = $pdf->getPageDimensions();
 $pdf->SetAutoPageBreak(true, 5);
 $pdf->SetFont('dejavusans');
 
+
 $inspection = $suket->inspection;
-$tanggal_inspeksi_raw = _d($inspection->date);
-$tanggal_suket_raw = $suket->licence_items[0]['tanggal_suket'];
-$expired_suket_raw = $suket->licence_items[0]['expired'];
+$equipment = $suket->equipment[0];
+$licence_item = $suket->licence_items[0];
+
 $client = $suket->client;
 $client_company = $client->company;
 $client_address = $client->address;
@@ -23,53 +24,46 @@ $client_company = $client->company;
 $client_address = $client->address;
 
 $equipment = $suket->equipment[0];
-$equipment_jenis_pesawat = $equipment['jenis_pesawat'];
-$equipment_lokasi = $equipment['lokasi'];
-$equipment_nama_pesawat = $equipment['nama_pesawat'];
-$equipment_pabrik_pembuat = $equipment['pabrik_pembuat'];
-$equipment_tahun_pembuatan = $equipment['tahun_pembuatan'];
+$equipment_jenis_pesawat = isset($equipment['jenis_pesawat']) ? $equipment['jenis_pesawat'] : '';
+$equipment_lokasi = isset($equipment['lokasi']) ? $equipment['lokasi'] : '';
+$equipment_nama_pesawat = isset($equipment['nama_pesawat']) ? $equipment['nama_pesawat'] : '';
+$equipment_pabrik_pembuat = isset($equipment['pabrik_pembuat']) ? $equipment['pabrik_pembuat'] : '';
+$equipment_tempat_pembuatan = isset($equipment['tempat_pembuatan']) ? $equipment['tempat_pembuatan'] : '';
+$equipment_tahun_pembuatan = isset($equipment['tahun_pembuatan']) ? $equipment['tahun_pembuatan'] : '';
 
-$equipment_nomor_seri = $equipment['nomor_seri'];
-$equipment_nomor_unit = $equipment['nomor_unit'];
-$equipment_kapasitas =$equipment['kapasitas'];
-$equipment_isi_cairan =$equipment['isi_cairan'];
-$equipment_digunakan_untuk = $equipment['digunakan_untuk'];
-$equipment_jenis_pemeriksaan = $equipment['jenis_pemeriksaan'];
-$equipment_jenis_bejana = $equipment['jenis_bejana'];
+$equipment_nomor_seri = isset($equipment['nomor_seri']) ? $equipment['nomor_seri'] : '';
+$equipment_nomor_unit = isset($equipment['nomor_unit']) ? $equipment['nomor_unit'] : '';
+$equipment_kapasitas = isset($equipment['kapasitas']) ? $equipment['kapasitas'] : '';
+$equipment_tekanan_design = isset($equipment['tekanan_design']) ? $equipment['tekanan_design'] : '';
+$equipment_tekanan_uji = isset($equipment['tekanan_uji']) ? $equipment['tekanan_uji'] : '';
+$equipment_jenis_pemeriksaan = isset($equipment['jenis_pemeriksaan']) ? $equipment['jenis_pemeriksaan'] : '';
+$equipment_bentuk = isset($equipment['bentuk']) ? $equipment['bentuk'] : '';
 $office_dinas = $suket->office->dinas;
-$regulasi = explode(' AND ', $equipment['regulasi']);
+$regulasi = explode(' -- ', $equipment['regulasi']);
 $equipment_regulasi = '';
 $equipment_regulasi .= '<ol class="regulasi">'; 
 
-$tahun = getYear($tanggal_inspeksi_raw);
-$bulan = getMonth($tanggal_inspeksi_raw);
-$tanggal = getDay($tanggal_inspeksi_raw);
-$tanggal_inspeksi = $tanggal.' '.$bulan.' '.$tahun;
+$tanggal_pemeriksaan = tanggal_pemeriksaan($inspection->date);
+$tanggal_suket = tanggal_suket($licence_item->tanggal_suket);
+$expired = tanggal_suket($licence_item->expired);
 
-$tahun = getYear($expired_suket_raw);
-$bulan = getMonth($expired_suket_raw);
-$tanggal = getDay($expired_suket_raw);
-$bulan_suket = $bulan.' '.$tahun;
-$expired = $tanggal.' '.$bulan.' '.$tahun;
+$nomor_suket = $licence_item->nomor_suket;
 
-
-$tahun = getYear($tanggal_suket_raw);
-$bulan = getMonth($tanggal_suket_raw);
-$tanggal = getDay($tanggal_suket_raw);
-$tanggal_suket = $tanggal.' '.$bulan.' '.$tahun;
-
-$nomor_suket = $suket->licence_items[0]['nomor_suket'];
-
-$text = 'SURAT KETERANGAN' ."\r\n";
-$text .= 'HASIL PEMERIKSAAN DAN PENGUJIAN' ."\r\n";
+$text = '<div style="text-align:center;"><strong>';
+$text .= 'SURAT KETERANGAN' . '<br>';
+$text .= '<span style="text-decoration: underline;">HASIL PEMERIKSAAN DAN PENGUJIAN' .'</span><br>';
 $text .= 'Nomor : ' . $nomor_suket;
+$text .= '</strong></div>';
 
+$pdf->setFontSize('12');
 $pdf->ln(20);
-$pdf->Write(0, $text, '', 0, 'C', true, 0, false, false, 0);
+$pdf->writeHTML($text, true, 0, true, true);
+
+$pdf->setFontSize('10');
 
 $text = 'Berdasarkan hasil pemeriksaan dan pengujian yang dilakukan oleh ';
 $text .= get_option('invoice_company_name');
-$text .= ' pada tanggal ' .$tanggal_inspeksi. ' terhadap ' .$equipment_nama_pesawat. ' dapat diterangkan bahwa :' . "\r\n";
+$text .= ' pada tanggal ' .$tanggal_pemeriksaan. ' terhadap ' .$equipment_nama_pesawat. ' dapat diterangkan bahwa :' . "\r\n";
 
 $pdf->SetLeftMargin(24);
 $pdf->SetRightMargin(24);
@@ -79,6 +73,7 @@ $pdf->Write(0, $text, '', 0, 'J', true, 0, false, false, 0);
 foreach($regulasi as $row){
     $equipment_regulasi .= '<li style="margin-left:70;">' .$row. '</li>'; 
 }
+
 $equipment_regulasi .= '</ol>'; 
 
 //var_dump($office_short_name);
@@ -142,23 +137,41 @@ $html = <<<EOD
         <td style="width:20;"></td>
         <td style="width:280;">2. Tempat / tahun pembuatan</td>
         <td style="width:10;">:</td>
-        <td style="width:420;"> / $equipment_tahun_pembuatan</td>
+        <td style="width:420;"> $equipment_tempat_pembuatan / $equipment_tahun_pembuatan</td>
     </tr>
     <tr>
         <td style="width:20;"></td>
-        <td style="width:280;">3. No. serie / No. unit</td>
+        <td style="width:280;">3. Bentuk</td>
+        <td style="width:10;">:</td>
+        <td style="width:420;">$equipment_bentuk</td>
+    </tr>
+    <tr>
+        <td style="width:20;"></td>
+        <td style="width:280;">4. No. serie / No. unit</td>
         <td style="width:10;">:</td>
         <td style="width:420;">$equipment_nomor_seri / $equipment_nomor_unit</td>
     </tr>
     <tr>
         <td style="width:20;"></td>
-        <td style="width:280;">4. Isi cairan</td>
+        <td style="width:280;">5. Kapasitas</td>
         <td style="width:10;">:</td>
-        <td style="width:420;">$equipment_isi_cairan</td>
+        <td style="width:420;">$equipment_kapasitas</td>
     </tr>
     <tr>
         <td style="width:20;"></td>
-        <td style="width:280;">5. Jenis pemeriksaan</td>
+        <td style="width:280;">6. Tekanan Design</td>
+        <td style="width:10;">:</td>
+        <td style="width:420;">$equipment_tekanan_design</td>
+    </tr>
+    <tr>
+        <td style="width:20;"></td>
+        <td style="width:280;">7. Tekanan Uji</td>
+        <td style="width:10;">:</td>
+        <td style="width:420;">$equipment_tekanan_uji</td>
+    </tr>
+    <tr>
+        <td style="width:20;"></td>
+        <td style="width:280;">8. Jenis pemeriksaan</td>
         <td style="width:10;">:</td>
         <td style="width:420;">$equipment_jenis_pemeriksaan</td>
     </tr>
@@ -202,7 +215,7 @@ $html = <<<EOD
     </tr>
     <tr>
         <td style="width:20;"></td>
-        <td style="width:650;">Berdasarkan hasil peneriksaan dan pengujian tersebut $equipment_nama_pesawat</td>
+        <td style="width:650;">Berdasarkan hasil pemeriksaan dan pengujian tersebut <strong>$equipment_nama_pesawat</strong></td>
         <td style="width:5;"></td>
         <td style="width:5;"></td>
     </tr>
@@ -230,20 +243,23 @@ $blank_line ="\r\n";
 $pdf->Write(0, $blank_line, '', 0, 'J', true, 0, false, false, 0);
 
 
-$text = 'MEMENUHI' ."\r\n";
-$text .= 'PERSYARATAN KESELAMATAN DAN KESEHATAN KERJA' ."\r\n";
+$text = '<div style="text-align:center;"><strong>';
+$text .= 'MEMENUHI' ."<br />";
+$text .= 'PERSYARATAN KESELAMATAN DAN KESEHATAN KERJA';
+$text .= '</strong></div>';
+$pdf->writeHTML($text, true, 0, true, true);
 
-$pdf->Write(0, $text, '', 0, 'C', true, 0, false, false, 0);
+$pdf->ln(4);
 
-$pdf->ln(5);
+$text = '<div style="text-align:justify;">';
+$text .= 'Demikian surat keterangan ini dibuat dengan sebenarnya agar dapat digunakan sebagaimana mestinya dan berlaku sepanjang objek pengujian tidak dilakukan perubahan dan / atau sampai dilakukan pengujian selanjutnya paling lambat tanggal ';
+$text .= '<strong>'. $expired .'</strong>.';
+$text .= '</div>';
+$pdf->SetLeftMargin(20);
+$pdf->SetRightMargin(20);
+$pdf->writeHTML($text, true, 0, true, true);
 
-$text = "Demikian surat keterangan ini dibuat dengan sebenarnya agar dapat digunakan sebagaimana mestinya dan berlaku sepanjang objek pengujian tidak dilakukan perubahan dan / atau sampai dilakukan pengujian selanjutnya paling lambat tanggal ";
-$text .= $expired .".\r\n \r\n";
-
-$pdf->SetLeftMargin(24);
-$pdf->SetRightMargin(24);
-$pdf->Write(0, $text, '', 0, 'J', true, 0, false, false, 0);
-
+$pdf->ln(4);
 
 $left_info = '<div style="text-align:center;">';
 $left_info .= "<br />";
@@ -256,14 +272,16 @@ $left_info .= "<br />";
 $left_info .= "<br />";
 $left_info .= "<br />";
 $left_info .= "<br />";
-$left_info .= $suket->office->kepala_dinas_nama;
 $left_info .= "<br />";
-$left_info .= $suket->office->kepala_dinas_nip;
+$left_info .= '<span style="text-decoration: underline;"><strong>' . $licence_item->kepala_dinas_nama . '</strong></span>';
+$left_info .= "<br />";
+$left_info .= '<strong>' . $licence_item->kepala_dinas_nip .'</strong>';
 $left_info .= '</div>';
 
 
 $right_info = '<div style="text-align:center;">';
 $right_info .= "Serang, $tanggal_suket" .'<br />';
+$right_info .= "<br />";
 $right_info .= 'Yang Melakukan Evaluasi,' .'<br />';
 $right_info .= 'Pengawas Ketenagakerjaan' .'<br />';
 $right_info .= "<br />";
@@ -273,11 +291,22 @@ $right_info .= "<br />";
 $right_info .= "<br />";
 $right_info .= "<br />";
 $right_info .= "<br />";
-$right_info .= $suket->office->pengawas_pubt_nama;
+$right_info .= '<span style="text-decoration: underline;"><strong>' . $licence_item->pengawas_nama . '</strong></span>';
 $right_info .= "<br />";
-$right_info .= $suket->office->pengawas_pubt_nip;
+$right_info .= '<strong>' . $licence_item->pengawas_nip . '</strong>';
 $right_info .= '</div>';
 
 
 
-pdf_multi_row($left_info, $right_info, $pdf, ($dimensions['wk'] / 2) - $dimensions['lm']);
+$html = <<<EOD
+
+<table cellspacing="1" cellpadding="1" border="0">
+    <tr>
+        <td style="text-align:center;">$left_info</td>
+        <td style="text-align:center;">$right_info</td>
+    </tr>
+</table>
+EOD;
+
+$pdf->writeHTML($html, true, 0, true, true);
+
