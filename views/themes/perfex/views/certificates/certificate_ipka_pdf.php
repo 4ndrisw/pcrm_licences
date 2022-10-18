2e1a6651d64e7e930cca13e7b301709f28d59d21
4ndr1sw <4ndr1sw@gmail.com>
2022-10-18 09:12:11 +0700

    Update certificate_ipka_pdf.php

--- views/themes/perfex/views/certificates/certificate_ipka_pdf.php
+++ views/themes/perfex/views/certificates/certificate_ipka_pdf.php
@@ -38,9 +38,20 @@ $equipment_jumlah_heat_detector = isset($equipment['jumlah_heat_detector']) ? $e
 $equipment_jumlah_titik_manggil_manual = isset($equipment['jumlah_titik_manggil_manual']) ? $equipment['jumlah_titik_manggil_manual'] : 'CEK DATA INSPEKSI';
 
 $office_dinas = $certificate->office->dinas;
-$regulasi = explode(' -- ', $equipment['regulasi']);
-$equipment_regulasi = '';
-$equipment_regulasi .= '<ol class="regulasi">'; 
+
+$default_regulation = get_option('predefined_regulation_of_'.$certificate->categories);
+$equipment_regulasi = !empty($certificate->inspection->equipment['regulasi']) ? $certificate->inspections->equipment['regulasi'] : $default_regulation;
+
+if (!empty($equipment_regulasi)) {
+    $regulasi = explode(' -- ', $equipment_regulasi);
+    $equipment_regulasi = '';
+    $equipment_regulasi .= '<ol class="regulasi">'; 
+
+    foreach($regulasi as $row){
+        $equipment_regulasi .= '<li style="margin-left:70;">' .$row. '</li>'; 
+    }
+    $equipment_regulasi .= '</ol>';
+}
 
 $tanggal_inspeksi_raw = isset($inspection->date) ? _d($inspection->date) : '1970-01-01';
 $tahun = getYear($tanggal_inspeksi_raw);
@@ -53,14 +64,6 @@ $tahun = getYear($tanggal_inspeksi_raw);
 $bulan = getMonth($tanggal_inspeksi_raw);
 $tanggal = getDay($tanggal_inspeksi_raw);
 $proposed_date = $tanggal.' '.$bulan.' '.$tahun;
-
-
-foreach($regulasi as $row){
-    $equipment_regulasi .= '<li style="margin-left:70;">' .$row. '</li>'; 
-}
-$equipment_regulasi .= '</ol>'; 
-
-//var_dump($certificate->equipment);
 
 // Set some content to print
 $pdf->ln(2);
