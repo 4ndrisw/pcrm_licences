c2154ed7887c477f5d984fc5bf7a35a8af1e637e
4ndr1sw <4ndr1sw@gmail.com>
2022-10-26 09:11:48 +0700

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -17,8 +17,8 @@ $route['licences/proposed/(:num)/(:any)'] = 'mylicence/show/$1/$2';
 //$route['licences/(:num)/suket/(:num)'] = 'licences/suket_item/$1/$2';
 
 
-//$route['licences/(:num)/sertifikat/pdf/(:num)'] = 'mylicence/certificate_pdf/$1/$2';
-//$route['licences/(:num)/suket/pdf/(:num)'] = 'mylicence/suket_pdf/$1/$2';
+$route['licences/(:num)/sertifikat/pdf/(:num)'] = 'mylicence/certificate_pdf/$1/$2';
+$route['licences/(:num)/suket/pdf/(:num)'] = 'mylicence/suket_pdf/$1/$2';
 
 $route['licences/pdf/(:num)'] = 'mylicence/pdf/$1';
 
